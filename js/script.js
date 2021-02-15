f1b9d1b7ce57465d8597610a849586ed6e8ea802
davide <davidecorsinimilano@gmail.com>
2021-02-15 12:56:06 +0100

    Push initial structure divided in colors

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// / VueJsAggiungere 
+// Header e Footer con VueJs
+// creando una struttura dati che consenta di inserire i link
+// nelle navbar.
+// Su questo non ci sono API da interrogare, 
+// ma create i dati a mano nelvostro js
