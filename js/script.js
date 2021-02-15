019b70acfdb19699a3276c76a95804c657433075
davide <davidecorsinimilano@gmail.com>
2021-02-15 16:35:10 +0100

    push initial header

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@
 // nelle navbar.
 // Su questo non ci sono API da interrogare, 
 // ma create i dati a mano nelvostro js
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        phone: '+1 (305) 1234-5678',
+        mail: 'hello@example.com', 
+        ciao: 'come stai',
+        profile: '<i class="fas fa-search"></i>',
+        arrayNav: [
+            'Home', 'Services', 'About', 'Pricing', 'come stamparlo?' , 'Get in touch'
+        ]
+    },
+    created(){
+        console.log(this.arrayHeader);
+    }
+})
