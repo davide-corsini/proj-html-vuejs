2468e7617592bea8c0718d4d806c79137dd5fa1f
davide <davidecorsinimilano@gmail.com>
2021-02-16 17:09:02 +0100

    near to end

--- js/script.js
+++ js/script.js
@@ -8,35 +8,100 @@
 var app = new Vue({
     el: '#app',
     data: {
+        ciao: '',
         phone: '+1 (305) 1234-5678',
         mail: 'hello@example.com', 
         map: 'Main Avenue, 987',
-        princingArray: [
-            {
-                titlePrice: ['Standard' , 'Home', 'Ultimate'],
-            },{
-                iClass: ['fas fa-rocket', 'fas fa-briefcase', 'fas fa-gem']
+        // princingArray: [
+        //     {
+        //         titlePrice: ['Standard' , 'Home', 'Ultimate'],
+        //     },{
+        //         iClass: ['fas fa-rocket', 'fas fa-briefcase', 'fas fa-gem']
 
-            },
-            {
-                contract: ['National Coverage', 'Unlimited Motoboy', 'Address Collection', 'Mobile App Tracking', 'Cargo Insurance']
-            }
-        ],
+        //     },
+        //     {
+        //         contract: ['National Coverage', 'Unlimited Motoboy', 'Address Collection', 'Mobile App Tracking', 'Cargo Insurance']
+        //     }
+        // ],
+        pulse: false,
+        active: '',
+        bounce: document.getElementsByClassName('bounce'),
+        whiteI : '',
         
-        
+        // part of pricing
         titlePrice: ['Standard', 'Home', 'Ultimate'],
-            
         iClass: ['fas fa-rocket', 'fas fa-briefcase', 'fas fa-gem'],
-
         standard: '490',
         professional: '890',
         ultimate: '1390',    
-            
         contract: ['National Coverage' , 'Unlimited Motoboy', 'Address Collection', 'Mobile App Tracking', 'Cargo Insurance'],         
-        
         arrayNav: ['Home', 'Services', 'About', 'Pricing'],
+
+        //form
+        name: '',
+        yourMail: '',
+        info: '',
+        phoneNumber: '',
+        discussion: '',
+        modelWindow: ''
+
     },
-    created(){
-        console.log(this.arrayHeader);
-    }
+    methods:{
+        scrollDown(){
+                if (window.scrollY > 0) {
+                    ciao.setAttribute(
+                        'style',
+                        'background-color: red;'
+                    )
+                }
+            
+        },
+        pulseArrow(){
+            // this.pulse = index;
+            //come gli passo sto index?
+            this.pulse = !this.pulse;
+            if(this.pulse == true){
+                this.bounce = 'bounce';
+                this.active = 'block';
+                this.whiteI = 'white-i';
+                setTimeout(() =>{
+                    this.active = '';
+                    this.bounce = '';
+                    this.whiteI = '';
+
+                },1500)
+            }
+            else{
+                this.active = '';
+                this.bounce = '';
+                this.whiteI = '';
+            }
+        },
+        pulseBack(){
+            return !this.active;
+        },
+        funzioneTempo(){
+            setTimeout(() => {
+                this.contacts[index].online = true;
+
+
+            }, 100);
+        },
+        sendForm(){
+            this.modelWindow = !this.modelWindow;
+            if(this.name.length > 0 && this.yourMail.length > 0){
+                console.log('funziona');
+                alert(this.name + this.surname);
+
+            }
+        }
+        }
+        // onBounce(){
+        //     this.pulse = !this.pulse;
+        //     if(this.pulse = true){
+        //     }
+        //     else{
+        //     }
+        // }
+    
 })
